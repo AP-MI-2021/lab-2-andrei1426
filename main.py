1cdb7e166f7ed269de634c10b70b69a418a36708
Andrei <andreigombos5@gmail.com>
2021-10-25 13:50:03 +0300

    lab2

--- main.py
+++ main.py
@@ -54,16 +54,32 @@ def test_is_palidnrome():
     assert is_palindrome(101) == True
 
 
+def get_leap_years(start,   end):
+    lista_ani = []
+    inceput=int(start)
+    sfarsit=int(end)
+    for x in range(inceput, sfarsit+1):
+        if x%4==0:
+            lista_ani.append(x)
+    return lista_ani
+
+
+def test_get_leap_years():
+    assert get_leap_years(2000,2008) == [2000, 2004, 2008]
+    assert get_leap_years(2004, 2016) == [2004, 2008, 2012, 2016]
+
+
 def print_menu():
     print("1. Test nr palindrom")
     print("2. Combinari de n luate cate k")
-    print("3. Iesire")
+    print("3. Afișează toți anii bisecți între doi ani dați (inclusiv anii dați).")
+    print("4. Iesire")
 
 
 def main():
     test_get_n_choose_k()
     test_is_palidnrome()
-
+    test_get_leap_years()
     while True:
         print_menu()
         optiune = input("Alegeti optiunea")
@@ -71,10 +87,14 @@ def main():
             n = int(input("Ce numar doriti sa testati?"))
             print(is_palindrome(n))
         elif optiune == "2":
-            n = int(input("Dati n="))
-            k = int(input("Dati k="))
+            n = input("Dati n=")
+            k = input("Dati k=")
             print(get_n_choose_k(n, k))
         elif optiune == "3":
+            start=input("extremitate initiale=")
+            end=input("extremitate finala=")
+            print(get_leap_years(start,end))
+        elif optiune == "4":
             break
         else:
             print("optiune greita!")
